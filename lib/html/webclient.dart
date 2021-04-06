5f5953458cfbe5e679441bbe985f46afffdce26b
busatoju <busatoju@gmail.com>
2021-04-06 14:02:40 -0300

    Adiicionada duração de 5segundos

--- lib/html/webclient.dart
+++ lib/html/webclient.dart
@@ -3,6 +3,8 @@ import 'package:http_interceptor/http_interceptor.dart';
 
 import 'web_interceptors/web_interceptor.dart';
 
-Client client =
-    HttpClientWithInterceptor.build(interceptors: [LoggingInterceptor()]);
+Client client = HttpClientWithInterceptor.build(
+  interceptors: [LoggingInterceptor()],
+  requestTimeout: Duration(seconds: 5),
+);
 var url = Uri.parse('http://192.168.18.5:8080/transactions');
